06d7c460bba453164496c0f9553a9d38581cd96a
Heiko Schabert <heiko.schabert@siemens.com>
2025-01-07 08:35:01 +0100

    chore: add metric endpoint
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- UpdateHub/Program.cs
+++ UpdateHub/Program.cs
@@ -12,6 +12,7 @@ using YamlDotNet.Serialization.NamingConventions;
 using YamlDotNet.Serialization;
 using System;
 using System.IO.Enumeration;
+using OpenTelemetry.Metrics;
 
 var CONFIG_FILE_PATH = Environment.GetEnvironmentVariable("CONFIG_FILE_PATH") ?? "./config.yaml";
 
@@ -30,6 +31,19 @@ Log.Debug(parser.ToString());
 // Configure web application
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
+builder.Services.AddOpenTelemetry().WithMetrics(builder =>
+{
+  builder.AddPrometheusExporter();
+
+  builder.AddMeter("Microsoft.AspNetCore.Hosting",
+    "Microsoft.AspNetCore.Server.Kestrel");
+  builder.AddView("http.server.request.duration",
+    new ExplicitBucketHistogramConfiguration
+    {
+      Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+        0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+    });
+});
 
 builder.Services.AddSingleton(parser.aasServerRepository);
 builder.Services.AddEndpointsApiExplorer();
@@ -51,6 +65,9 @@ builder.Services.AddHttpClient(string.Empty).ConfigureHttpClient(c => {
 
 var app = builder.Build();
 app.MapHealthChecks("/healthz");
+
+if (System.Environment.GetEnvironmentVariable("ENABLE_METRIC") == "true")
+  app.MapPrometheusScrapingEndpoint();
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>

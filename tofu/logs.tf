786c77c13d3b341570627e1345a3b8d2682ba203
Florian Gerdes <florian.gerdes@siemens.com>
2025-01-15 11:02:39 +0100

    build(tofu+.gitlab-ci): tofu only used for s3 + deploy deleted

--- tofu/logs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "irs_log_group" {
-  name              = "/ecs/irs"
-  retention_in_days = 30
-}
-
-resource "aws_cloudwatch_log_stream" "cb_log_stream" {
-  name           = "irs-log-stream"
-  log_group_name = aws_cloudwatch_log_group.irs_log_group.name
-}
--- tofu/variables.tf
+++ tofu/variables.tf
@@ -3,41 +3,6 @@ variable "config_file_path" {
   default = "./config.yaml"
 }
 
-//---------------------------------------------------------------------
-
-variable "github_pat_username" {
-  type = string
-}
-
-variable "github_pat_token" {
-  type      = string
-  sensitive = true
-}
-
-//---------------------------------------------------------------------
-
-variable "domain_name" {
-  type    = string
-  default = "cdm-dev.xo.siemens.cloud"
-}
-
-//-----------------------------------------------------------------------
-
-variable "vpc_id" {
-  description = "The ID of the existing VPC."
-  type        = string
-}
-
-variable "public_subnet_ids" {
-  description = "The IDs of the existing subnets."
-  type        = list(string)
-}
-
-variable "private_subnet_ids" {
-  description = "The IDs of the existing subnets."
-  type        = list(string)
-}
-
 //-----------------------------------------------------------------------
 
 variable "repository" {

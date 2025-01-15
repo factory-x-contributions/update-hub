af41863b4805cc25a130db30ea70647c81b755a1
Heiko Schabert <heiko.schabert@siemens.com>
2025-01-15 13:17:55 +0100

    chore: remove unused IaC deployment
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- tofu/import.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "scaffold-infra" {
-  backend = "s3"
-
-  config = {
-    bucket  = "state-bucket-${data.aws_caller_identity.current.account_id}"
-    key     = "${var.repository}.tfstate"
-  }
-}

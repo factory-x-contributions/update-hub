25ae3f92c29f1d2fa217d016f2431ca16f916fed
Heiko Schabert <heiko.schabert@siemens.com>
2025-01-16 12:42:20 +0100

    chore: add flakes
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- shell.nix
+++ shell.nix
@@ -1,21 +1,23 @@
-{pkgs ? import <nixpkgs> {}}:
-with pkgs;
-  mkShell {
-    nativeBuildInputs = [
-      (python3.withPackages (ps: with ps; [pyyaml]))
-      git
-      just
-
-      mono
-      msbuild
-      dotnet-sdk_8
-
-      pre-commit
-      shellcheck
-      prettier
-      markdownlint-cli
-    ];
-
-    shellHook = ''
-    '';
+# This is a shell.nix file used to describe the environment that is needed
+# to develop.
+#
+# For more information about this and why this file is useful, see here:
+# https://nixos.org/guides/nix-pills/developing-with-nix-shell.html
+#
+# Also have a look into direnv: https://direnv.net/, this can make it so that you can
+# automatically get your environment set up when you change folders into the
+# project.
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
   }
+).shellNix
